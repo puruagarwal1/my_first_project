aafbc62c979119ccaf3c587c7c43321d78beb3c8
puruagarwal1 <92991672+puruagarwal1@users.noreply.github.com>
2021-10-22 19:21:53 +0530

    Amstrong C code

---
+++ armstrong till n.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main (){
+	int num,count = 1,rem,sum,a;
+	printf("enter a number:");
+
+	fflush(stdin);
+		scanf("%d",&a);
+
+	while(count <=a){
+		num=count;
+		sum=0;
+		
+		while(num>0){
+			rem = num % 10;
+			sum = sum + (rem*rem*rem);
+			num = num / 10;
+		}
+		//printf("num=%d,sum=%d ",count,sum);
+		if(sum==count)
+			printf("\n%d",count);
+		
+		count++;
+	}
+	return 0;
+}
